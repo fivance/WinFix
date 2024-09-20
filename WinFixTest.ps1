a15ac34e2225a384d17b03656583048b23d82c7f
fivance <fran.ivancevic7@gmail.com>
2024-09-20 16:25:40 +0200

    Add files via upload

--- WinFixTest.ps1
+++ WinFixTest.ps1
@@ -16,15 +16,37 @@ Write-Host "Press Enter to continue:"
 Read-Host
 
 function Get-FileFromWeb {
-    param (
-        # Parameter help description
-        [Parameter(Mandatory)]
-        [string]$URL,
-  
-        # Parameter help description
-        [Parameter(Mandatory)]
-        [string]$File 
-    )
+    param ([Parameter(Mandatory)][string]$URL, [Parameter(Mandatory)][string]$File)
+    function Show-Progress {
+    param ([Parameter(Mandatory)][Single]$TotalValue, [Parameter(Mandatory)][Single]$CurrentValue, [Parameter(Mandatory)][string]$ProgressText, [Parameter()][int]$BarSize = 10, [Parameter()][switch]$Complete)
+    $percent = $CurrentValue / $TotalValue
+    $percentComplete = $percent * 100
+    if ($psISE) { Write-Progress "$ProgressText" -id 0 -percentComplete $percentComplete }
+    else { Write-Host -NoNewLine "`r$ProgressText $(''.PadRight($BarSize * $percent, [char]9608).PadRight($BarSize, [char]9617)) $($percentComplete.ToString('##0.00').PadLeft(6)) % " }
+    }
+    try {
+    $request = [System.Net.HttpWebRequest]::Create($URL)
+    $response = $request.GetResponse()
+    if ($response.StatusCode -eq 401 -or $response.StatusCode -eq 403 -or $response.StatusCode -eq 404) { throw "Remote file either doesn't exist, is unauthorized, or is forbidden for '$URL'." }
+    if ($File -match '^\.\\') { $File = Join-Path (Get-Location -PSProvider 'FileSystem') ($File -Split '^\.')[1] }
+    if ($File -and !(Split-Path $File)) { $File = Join-Path (Get-Location -PSProvider 'FileSystem') $File }
+    if ($File) { $fileDirectory = $([System.IO.Path]::GetDirectoryName($File)); if (!(Test-Path($fileDirectory))) { [System.IO.Directory]::CreateDirectory($fileDirectory) | Out-Null } }
+    [long]$fullSize = $response.ContentLength
+    [byte[]]$buffer = new-object byte[] 1048576
+    [long]$total = [long]$count = 0
+    $reader = $response.GetResponseStream()
+    $writer = new-object System.IO.FileStream $File, 'Create'
+    do {
+    $count = $reader.Read($buffer, 0, $buffer.Length)
+    $writer.Write($buffer, 0, $count)
+    $total += $count
+    if ($fullSize -gt 0) { Show-Progress -TotalValue $fullSize -CurrentValue $total -ProgressText " $($File.Name)" }
+    } while ($count -gt 0)
+    }
+    finally {
+    $reader.Close()
+    $writer.Close()
+    }
     }
  
  Write-Host "Select an option:"
@@ -2856,4 +2878,10 @@ exit
     irm christitus.com/win | iex
 
    }
-    } } else { Write-Host "Invalid input. Please select a valid option (1-6)." } }
+    } } else { Write-Host "Invalid input. Please select a valid option (1-6)." 
+    Write-Host "Press Enter to continue..."
+    Read-Host
+    Clear-Host
+     } }
+   
+  
