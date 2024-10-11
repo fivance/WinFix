d097814e82e3142b13edf1a49e44cb1c83225e85
fivance <fran.ivancevic7@gmail.com>
2024-10-11 13:29:24 +0200

    added icon size small tweak

--- WinFixFinal2.ps1
+++ WinFixFinal2.ps1
@@ -2418,6 +2418,21 @@ Write-Host "Set Lockscreen to black"
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "UseCompactMode" /t REG_DWORD /d "1" /f | Out-Null
 Clear-Host
 
+# Change desktop icon size to small (value: 32 for small icons)
+$desktopIconSizeKey = "HKCU:\Software\Microsoft\Windows\Shell\Bags\1\Desktop"
+$iconSizeValue = 32
+
+# Create the registry key path if it doesn't exist
+If (-not (Test-Path $desktopIconSizeKey)) {
+    New-Item -Path $desktopIconSizeKey -Force
+}
+
+# Set the Icon Size for desktop
+Set-ItemProperty -Path $desktopIconSizeKey -Name IconSize -Value $iconSizeValue
+
+# Restart explorer.exe to apply changes
+Stop-Process -Name explorer -Force
+Start-Process explorer
 
 
 #OneDrive FileExplorer pin remove
