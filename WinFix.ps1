cde15d4184c91baaa22db34d8e9a0c4bcd71ed32
Fran <fran.ivancevic7@gmail.com>
2024-10-19 14:53:10 +0200

    padding fix

--- WinFix.ps1
+++ WinFix.ps1
@@ -9,69 +9,80 @@ Clear-Host
 
 function Set-ConsoleOpacity {
   param(
-    [ValidateRange(10, 100)]
-    [int]$Opacity
+      [ValidateRange(10, 100)]
+      [int]$Opacity
   )
 
   # Check if pinvoke type already exists, if not import the relevant functions
   try {
-    $Win32Type = [Win32.WindowLayer]
+      $Win32Type = [Win32.WindowLayer]
   }
   catch {
-    $Win32Type = Add-Type -MemberDefinition @'
-            [DllImport("user32.dll")]
-            public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
-
-            [DllImport("user32.dll")]
-            public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
-
-            [DllImport("user32.dll")]
-            public static extern bool SetLayeredWindowAttributes(IntPtr hwnd, uint crKey, byte bAlpha, uint dwFlags);
+      $Win32Type = Add-Type -MemberDefinition @'
+              [DllImport("user32.dll")]
+              public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+  
+              [DllImport("user32.dll")]
+              public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+  
+              [DllImport("user32.dll")]
+              public static extern bool SetLayeredWindowAttributes(IntPtr hwnd, uint crKey, byte bAlpha, uint dwFlags);
 '@ -Name WindowLayer -Namespace Win32 -PassThru
   }
   
-  #changes console to black
-function color ($bc, $fc) {
-    $a = (Get-Host).UI.RawUI
-    $a.BackgroundColor = $bc
-    $a.ForegroundColor = $fc 
-    Clear-Host 
-} 
+  # Changes console to black
+  function color ($bc, $fc) {
+      $a = (Get-Host).UI.RawUI
+      $a.BackgroundColor = $bc
+      $a.ForegroundColor = $fc 
+      Clear-Host 
+  } 
 
-color 'black' 'white'
+  color 'black' 'white'
 
-# Get the console window width
-$consoleWidth = $Host.UI.RawUI.WindowSize.Width
+  # Get the console window width
+  $consoleWidth = $Host.UI.RawUI.WindowSize.Width
 
-# Calculate the number of padding characters needed on each side
-$padding = ($consoleWidth - 21) / 2
+  # Ensure console width is sufficient
+  if ($consoleWidth -lt 21) {
+      Write-Host "Console width is too small for the title." -ForegroundColor Red
+      return
+  }
 
-# Create the full title string with padding
-$fullTitle = '-' * [Math]::Floor($padding) + ' ' + 'WinFix script' + ' ' + '-' * [Math]::Ceiling($padding)
+  # Calculate the number of padding characters needed on each side
+  $padding = [Math]::Floor(($consoleWidth - 21) / 2)
 
+  # Ensure padding is non-negative
+  if ($padding -lt 0) {
+      $padding = 0
+  }
 
-Write-Host $fullTitle -BackgroundColor White -ForegroundColor Black
+  # Create the full title string with padding
+  $fullTitle = '-' * $padding + ' WinFix script ' + '-' * $padding
+
+  Write-Host $fullTitle -BackgroundColor White -ForegroundColor Black
 
   # Calculate opacity value (0-255)
   $OpacityValue = [int]($Opacity * 2.56) - 1
 
-  # Grab the host windows handle
+  # Grab the host window's handle
   $ThisProcess = Get-Process -Id $PID
   $WindowHandle = $ThisProcess.MainWindowHandle
 
-  # "Constants"
-  $GwlExStyle = -20;
-  $WsExLayered = 0x80000;
-  $LwaAlpha = 0x2;
+  # Constants
+  $GwlExStyle = -20
+  $WsExLayered = 0x80000
+  $LwaAlpha = 0x2
 
   if ($Win32Type::GetWindowLong($WindowHandle, -20) -band $WsExLayered -ne $WsExLayered) {
-    # If Window isn't already marked "Layered", make it so
-    [void]$Win32Type::SetWindowLong($WindowHandle, $GwlExStyle, $Win32Type::GetWindowLong($WindowHandle, $GwlExStyle) -bxor $WsExLayered)
+      # If Window isn't already marked "Layered", make it so
+      [void]$Win32Type::SetWindowLong($WindowHandle, $GwlExStyle, $Win32Type::GetWindowLong($WindowHandle, $GwlExStyle) -bxor $WsExLayered)
   }
 
   # Set transparency
   [void]$Win32Type::SetLayeredWindowAttributes($WindowHandle, 0, $OpacityValue, $LwaAlpha)
 }
+
 
 Set-ConsoleOpacity -Opacity 90
 
@@ -4689,7 +4700,7 @@ if ($input) { exit }}
 
 
 10 { 
-  
+
   Clear-Host
   Write-Host "Exiting..."
   Start-Sleep -Seconds 2
