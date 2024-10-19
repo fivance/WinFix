602973fdd95686f387f06d069415ac540b787ec3
Fran <fran.ivancevic7@gmail.com>
2024-10-19 15:07:34 +0200

    possible Get-FileFromWeb function fix

--- WinFix.ps1
+++ WinFix.ps1
@@ -86,39 +86,79 @@ function Set-ConsoleOpacity {
 
 Set-ConsoleOpacity -Opacity 94
 
-function Get-FileFromWeb {
-param ([Parameter(Mandatory)][string]$URL, [Parameter(Mandatory)][string]$File)
 function Show-Progress {
-param ([Parameter(Mandatory)][Single]$TotalValue, [Parameter(Mandatory)][Single]$CurrentValue, [Parameter(Mandatory)][string]$ProgressText, [Parameter()][int]$BarSize = 10, [Parameter()][switch]$Complete)
-$percent = $CurrentValue / $TotalValue
-$percentComplete = $percent * 100
-if ($psISE) { Write-Progress "$ProgressText" -id 0 -percentComplete $percentComplete }
-else { Write-Host -NoNewLine "`r$ProgressText $(''.PadRight($BarSize * $percent, [char]9608).PadRight($BarSize, [char]9617)) $($percentComplete.ToString('##0.00').PadLeft(6)) % " }
+  param (
+      [Parameter(Mandatory)][Single]$TotalValue,
+      [Parameter(Mandatory)][Single]$CurrentValue,
+      [Parameter(Mandatory)][string]$ProgressText,
+      [Parameter()][int]$BarSize = 10,
+      [Parameter()][switch]$Complete
+  )
+  $percent = $CurrentValue / $TotalValue
+  $percentComplete = $percent * 100
+  if ($psISE) {
+      Write-Progress "$ProgressText" -Id 0 -PercentComplete $percentComplete
+  } else {
+      Write-Host -NoNewLine "`r$ProgressText $(''.PadRight($BarSize * $percent, [char]9608).PadRight($BarSize, [char]9617)) $($percentComplete.ToString('##0.00').PadLeft(6)) % "
+  }
 }
-try {
-$request = [System.Net.HttpWebRequest]::Create($URL)
-$response = $request.GetResponse()
-if ($response.StatusCode -eq 401 -or $response.StatusCode -eq 403 -or $response.StatusCode -eq 404) { throw "Remote file either doesn't exist, is unauthorized, or is forbidden for '$URL'." }
-if ($File -match '^\.\\') { $File = Join-Path (Get-Location -PSProvider 'FileSystem') ($File -Split '^\.')[1] }
-if ($File -and !(Split-Path $File)) { $File = Join-Path (Get-Location -PSProvider 'FileSystem') $File }
-if ($File) { $fileDirectory = $([System.IO.Path]::GetDirectoryName($File)); if (!(Test-Path($fileDirectory))) { [System.IO.Directory]::CreateDirectory($fileDirectory) | Out-Null } }
-[long]$fullSize = $response.ContentLength
-[byte[]]$buffer = new-object byte[] 1048576
-[long]$total = [long]$count = 0
-$reader = $response.GetResponseStream()
-$writer = new-object System.IO.FileStream $File, 'Create'
-do {
-$count = $reader.Read($buffer, 0, $buffer.Length)
-$writer.Write($buffer, 0, $count)
-$total += $count
-if ($fullSize -gt 0) { Show-Progress -TotalValue $fullSize -CurrentValue $total -ProgressText " $($File.Name)" }
-} while ($count -gt 0)
+
+function Get-FileFromWeb {
+  param (
+      [Parameter(Mandatory)][string]$URL,
+      [Parameter(Mandatory)][string]$File
+  )
+
+  [System.IO.FileStream]$writer = $null
+  [System.IO.Stream]$reader = $null
+
+  try {
+      $request = [System.Net.HttpWebRequest]::Create($URL)
+      $response = $request.GetResponse()
+
+      if ($response.StatusCode -eq 401 -or $response.StatusCode -eq 403 -or $response.StatusCode -eq 404) {
+          throw "Remote file either doesn't exist, is unauthorized, or is forbidden for '$URL'."
+      }
+
+      if ($File -match '^\.\\') {
+          $File = Join-Path (Get-Location -PSProvider 'FileSystem') ($File -Split '^\.')[1]
+      }
+
+      if ($File -and !(Split-Path $File)) {
+          $File = Join-Path (Get-Location -PSProvider 'FileSystem') $File
+      }
+
+      if ($File) {
+          $fileDirectory = [System.IO.Path]::GetDirectoryName($File)
+          if (!(Test-Path($fileDirectory))) {
+              [System.IO.Directory]::CreateDirectory($fileDirectory) | Out-Null
+          }
+      }
+
+      [long]$fullSize = $response.ContentLength
+      [byte[]]$buffer = New-Object byte[] 1048576
+      [long]$total = [long]$count = 0
+      $reader = $response.GetResponseStream()
+      $writer = New-Object System.IO.FileStream $File, 'Create'
+
+      do {
+          $count = $reader.Read($buffer, 0, $buffer.Length)
+          $writer.Write($buffer, 0, $count)
+          $total += $count
+          if ($fullSize -gt 0) {
+              Show-Progress -TotalValue $fullSize -CurrentValue $total -ProgressText "Downloading $([System.IO.Path]::GetFileName($File))"
+          }
+      } while ($count -gt 0)
+  }
+  catch {
+      Write-Host "An error occurred: $_" -ForegroundColor Red
+  }
+  finally {
+      if ($reader) { $reader.Close() }
+      if ($writer) { $writer.Close() }
+  }
 }
-finally {
-$reader.Close()
-$writer.Close()
-}
-}
+
 
 (Invoke-WebRequest "https://raw.githubusercontent.com/lptstr/winfetch/master/winfetch.ps1" -UseBasicParsing).Content.Remove(0,1) | Invoke-Expression
 
