9dc56cae7b6f5e735f07fa2302a5328f114c7632
fivance <fran.ivancevic7@gmail.com>
2024-10-22 12:05:52 +0200

    added always show extensions script under 12)

--- WinFix.ps1
+++ WinFix.ps1
@@ -183,7 +183,8 @@ Write-Host " 8. Winget fix"
 Write-Host " 9. Disable Recall and AI features"
 Write-Host " 10. Install StartAllBack and apply settings"
 Write-Host " 11. Set SystemLocale"
-Write-Host " 12. Exit script"
+Write-Host " 12. Always show extensions script"
+Write-Host " 13. Exit script"
               }
 
 while ($true) {
@@ -4817,10 +4818,70 @@ Start-Sleep -Seconds 3
 
 }
 
-
-
-
 12 { 
+
+  function Run-Trusted([String]$command) {
+
+    Stop-Service -Name TrustedInstaller -Force -ErrorAction SilentlyContinue
+    #get bin path to revert later
+    $service = Get-WmiObject -Class Win32_Service -Filter "Name='TrustedInstaller'"
+    $DefaultBinPath = $service.PathName
+    #convert command to base64 to avoid errors with spaces
+    $bytes = [System.Text.Encoding]::Unicode.GetBytes($command)
+    $base64Command = [Convert]::ToBase64String($bytes)
+    #change bin to command
+    sc.exe config TrustedInstaller binPath= "cmd.exe /c powershell.exe -encodedcommand $base64Command" | Out-Null
+    #run the command
+    sc.exe start TrustedInstaller | Out-Null
+    #set bin back to default
+    sc.exe config TrustedInstaller binpath= "`"$DefaultBinPath`"" | Out-Null
+    Stop-Service -Name TrustedInstaller -Force -ErrorAction SilentlyContinue
+
+}
+
+
+$value = Get-ItemPropertyValue -Path 'registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name HideFileExt -ErrorAction SilentlyContinue
+if ($value -ne 0 -or $null -eq $value) {
+    Write-Host 'Enabling Show File Extensions' -ForegroundColor Green
+    Reg.exe add 'HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' /v 'HideFileExt' /t REG_DWORD /d '0' /f *>$null
+}
+
+
+$regPath = 'registry::HKEY_CLASSES_ROOT'
+
+$items = (Get-ChildItem -Path $regPath).Name
+$neverShowExtPaths = @()
+foreach ($item in $items) {
+    $path = ((Get-ChildItem -Path "registry::$item" -Depth 0) | Where-Object { $_.Property -like 'NeverShowExt' }).Name
+    if ($path -ne $null) {
+        $neverShowExtPaths += $path
+    }
+}
+
+if ($neverShowExtPaths.Length -eq 0) {
+    Write-Host 'No Extensions Hidden!' -ForegroundColor Green
+}
+else {
+    $count = 0
+    foreach ($path in $neverShowExtPaths) {
+        if ($null -ne $path) {
+            $name = $path -replace 'HKEY_CLASSES_ROOT\\' , ''
+            Write-Host "Unhiding: $name" 
+            $count++
+            $command = "Remove-ItemProperty -Path `"registry::$path`" -Name 'NeverShowExt' -Force"
+            Run-Trusted -command $command
+        
+        }
+    }
+    Write-Host "$count File Extensions Unhidden!" -ForegroundColor Green
+    Start-Sleep -Seconds 2
+
+}
+
+}
+
+
+13 { 
 
   Clear-Host
   Write-Host "Exiting..."
