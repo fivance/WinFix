829baa106c09aeddd99b8c339bca05cd39d377ee
fivance <fran.ivancevic7@gmail.com>
2024-10-23 12:32:12 +0200

    modified uac disable script

--- WinFix.ps1
+++ WinFix.ps1
@@ -4898,7 +4898,7 @@ Start-Process explorer
 
 14 {
 Write-Host "Disabling UAC..."
-Start-Sleep -2
+Start-Sleep -Seconds 2
 Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name ConsentPromptBehaviorAdmin -Value 0
 $currentValue = Get-ItemProperty -Path "REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin"
 
