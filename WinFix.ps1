f68a944bbe7fb0b20f9e562bba72604886e87ff9
Fran Ivančević <fivancevic@team.nova.hr>
2024-10-25 12:27:43 +0200

    added solid color bacground option - persistent

--- WinFix.ps1
+++ WinFix.ps1
@@ -3135,6 +3135,33 @@ else {
   Remove-Item -Path "$env:TEMP\UpdateTaskTray" -Force -ErrorAction SilentlyContinue
   Write-Host 'Update Task Tray Created...New Apps Will Be Shown Upon Restarting'
 
+
+# Define the registry path
+$registryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager"
+
+# Disable "Learn more about this picture" for desktop background
+if (Test-Path $registryPath) {
+    Set-ItemProperty -Path $registryPath -Name "RotatingLockScreenEnabled" -Value 0
+    Set-ItemProperty -Path $registryPath -Name "RotatingLockScreenOverlayEnabled" -Value 0
+    Set-ItemProperty -Path $registryPath -Name "SubscribedContent-338389Enabled" -Value 0
+    Write-Output "'Learn more about this picture' has been disabled for the desktop background."
+} else {
+    Write-Output "The registry path for ContentDeliveryManager does not exist."
+}
+
+# Set wallpaper style to solid color (black)
+$registryPathDesktop = "HKCU:\Control Panel\Desktop"
+Set-ItemProperty -Path $registryPathDesktop -Name "Wallpaper" -Value ""
+Set-ItemProperty -Path $registryPathDesktop -Name "WallpaperStyle" -Value 0
+Set-ItemProperty -Path $registryPathDesktop -Name "BackgroundType" -Value 1
+Set-ItemProperty -Path $registryPathDesktop -Name "SingleColor" -Value "000000" # Black color in hex
+
+# Update the system parameters to apply changes
+RUNDLL32.EXE user32.dll, UpdatePerUserSystemParameters ,1 ,True
+Write-Output "Wallpaper style has been set to solid black."
+
+
+
 # Clear %temp% folder
 Remove-Item -Path "$env:USERPROFILE\AppData\Local\Temp" -Recurse -Force -ErrorAction SilentlyContinue | Out-Null
 New-Item -Path "$env:USERPROFILE\AppData\Local" -Name "Temp" -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
@@ -3142,6 +3169,7 @@ New-Item -Path "$env:USERPROFILE\AppData\Local" -Name "Temp" -ItemType Directory
 Remove-Item -Path "$env:SystemDrive\Windows\Temp" -Recurse -Force -ErrorAction SilentlyContinue | Out-Null
 New-Item -Path "$env:SystemDrive\Windows" -Name "Temp" -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
 # Open disk cleanup
+
 Start-Process cleanmgr.exe
 }  
 
