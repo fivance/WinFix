9e0c70c240ac63df04df18eb913ac100d6f3f09a
fivance <fran.ivancevic7@gmail.com>
2025-10-10 12:29:45 +0200

    added timer resolution

--- WinFix.ps1
+++ WinFix.ps1
@@ -3906,6 +3906,244 @@ function Enable-WSL {
     Write-Host "$ts - WSL configuration completed. A reboot is recommended." -ForegroundColor Yellow
 }
 
+function Install-TimerResolution {
+    Write-Host "1. Timer Resolution: On"
+    Write-Host "2. Timer Resolution: Off"
+    while ($true) {
+    $choice = Read-Host " "
+    if ($choice -match '^[1-2]$') {
+    switch ($choice) {
+    1 {
+
+Clear-Host
+Write-Host "Installing: Set Timer Resolution Service . . ."
+$MultilineComment = @"
+using System;
+using System.Runtime.InteropServices;
+using System.ServiceProcess;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Collections.Generic;
+using System.Reflection;
+using System.IO;
+using System.Management;
+using System.Threading;
+using System.Diagnostics;
+[assembly: AssemblyVersion("2.1")]
+[assembly: AssemblyProduct("Set Timer Resolution service")]
+namespace WindowsService
+{
+    class WindowsService : ServiceBase
+    {
+        public WindowsService()
+        {
+            this.ServiceName = "STR";
+            this.EventLog.Log = "Application";
+            this.CanStop = true;
+            this.CanHandlePowerEvent = false;
+            this.CanHandleSessionChangeEvent = false;
+            this.CanPauseAndContinue = false;
+            this.CanShutdown = false;
+        }
+        static void Main()
+        {
+            ServiceBase.Run(new WindowsService());
+        }
+        protected override void OnStart(string[] args)
+        {
+            base.OnStart(args);
+            ReadProcessList();
+            NtQueryTimerResolution(out this.MininumResolution, out this.MaximumResolution, out this.DefaultResolution);
+            if(null != this.EventLog)
+                try { this.EventLog.WriteEntry(String.Format("Minimum={0}; Maximum={1}; Default={2}; Processes='{3}'", this.MininumResolution, this.MaximumResolution, this.DefaultResolution, null != this.ProcessesNames ? String.Join("','", this.ProcessesNames) : "")); }
+                catch {}
+            if(null == this.ProcessesNames)
+            {
+                SetMaximumResolution();
+                return;
+            }
+            if(0 == this.ProcessesNames.Count)
+            {
+                return;
+            }
+            this.ProcessStartDelegate = new OnProcessStart(this.ProcessStarted);
+            try
+            {
+                String query = String.Format("SELECT * FROM __InstanceCreationEvent WITHIN 0.5 WHERE (TargetInstance isa \"Win32_Process\") AND (TargetInstance.Name=\"{0}\")", String.Join("\" OR TargetInstance.Name=\"", this.ProcessesNames));
+                this.startWatch = new ManagementEventWatcher(query);
+                this.startWatch.EventArrived += this.startWatch_EventArrived;
+                this.startWatch.Start();
+            }
+            catch(Exception ee)
+            {
+                if(null != this.EventLog)
+                    try { this.EventLog.WriteEntry(ee.ToString(), EventLogEntryType.Error); }
+                    catch {}
+            }
+        }
+        protected override void OnStop()
+        {
+            if(null != this.startWatch)
+            {
+                this.startWatch.Stop();
+            }
+
+            base.OnStop();
+        }
+        ManagementEventWatcher startWatch;
+        void startWatch_EventArrived(object sender, EventArrivedEventArgs e) 
+        {
+            try
+            {
+                ManagementBaseObject process = (ManagementBaseObject)e.NewEvent.Properties["TargetInstance"].Value;
+                UInt32 processId = (UInt32)process.Properties["ProcessId"].Value;
+                this.ProcessStartDelegate.BeginInvoke(processId, null, null);
+            } 
+            catch(Exception ee) 
+            {
+                if(null != this.EventLog)
+                    try { this.EventLog.WriteEntry(ee.ToString(), EventLogEntryType.Warning); }
+                    catch {}
+
+            }
+        }
+        [DllImport("kernel32.dll", SetLastError=true)]
+        static extern Int32 WaitForSingleObject(IntPtr Handle, Int32 Milliseconds);
+        [DllImport("kernel32.dll", SetLastError=true)]
+        static extern IntPtr OpenProcess(UInt32 DesiredAccess, Int32 InheritHandle, UInt32 ProcessId);
+        [DllImport("kernel32.dll", SetLastError=true)]
+        static extern Int32 CloseHandle(IntPtr Handle);
+        const UInt32 SYNCHRONIZE = 0x00100000;
+        delegate void OnProcessStart(UInt32 processId);
+        OnProcessStart ProcessStartDelegate = null;
+        void ProcessStarted(UInt32 processId)
+        {
+            SetMaximumResolution();
+            IntPtr processHandle = IntPtr.Zero;
+            try
+            {
+                processHandle = OpenProcess(SYNCHRONIZE, 0, processId);
+                if(processHandle != IntPtr.Zero)
+                    WaitForSingleObject(processHandle, -1);
+            } 
+            catch(Exception ee) 
+            {
+                if(null != this.EventLog)
+                    try { this.EventLog.WriteEntry(ee.ToString(), EventLogEntryType.Warning); }
+                    catch {}
+            }
+            finally
+            {
+                if(processHandle != IntPtr.Zero)
+                    CloseHandle(processHandle); 
+            }
+            SetDefaultResolution();
+        }
+        List<String> ProcessesNames = null;
+        void ReadProcessList()
+        {
+            String iniFilePath = Assembly.GetExecutingAssembly().Location + ".ini";
+            if(File.Exists(iniFilePath))
+            {
+                this.ProcessesNames = new List<String>();
+                String[] iniFileLines = File.ReadAllLines(iniFilePath);
+                foreach(var line in iniFileLines)
+                {
+                    String[] names = line.Split(new char[] {',', ' ', ';'} , StringSplitOptions.RemoveEmptyEntries);
+                    foreach(var name in names)
+                    {
+                        String lwr_name = name.ToLower();
+                        if(!lwr_name.EndsWith(".exe"))
+                            lwr_name += ".exe";
+                        if(!this.ProcessesNames.Contains(lwr_name))
+                            this.ProcessesNames.Add(lwr_name);
+                    }
+                }
+            }
+        }
+        [DllImport("ntdll.dll", SetLastError=true)]
+        static extern int NtSetTimerResolution(uint DesiredResolution, bool SetResolution, out uint CurrentResolution);
+        [DllImport("ntdll.dll", SetLastError=true)]
+        static extern int NtQueryTimerResolution(out uint MinimumResolution, out uint MaximumResolution, out uint ActualResolution);
+        uint DefaultResolution = 0;
+        uint MininumResolution = 0;
+        uint MaximumResolution = 0;
+        long processCounter = 0;
+        void SetMaximumResolution()
+        {
+            long counter = Interlocked.Increment(ref this.processCounter);
+            if(counter <= 1)
+            {
+                uint actual = 0;
+                NtSetTimerResolution(this.MaximumResolution, true, out actual);
+                if(null != this.EventLog)
+                    try { this.EventLog.WriteEntry(String.Format("Actual resolution = {0}", actual)); }
+                    catch {}
+            }
+        }
+        void SetDefaultResolution()
+        {
+            long counter = Interlocked.Decrement(ref this.processCounter);
+            if(counter < 1)
+            {
+                uint actual = 0;
+                NtSetTimerResolution(this.DefaultResolution, true, out actual);
+                if(null != this.EventLog)
+                    try { this.EventLog.WriteEntry(String.Format("Actual resolution = {0}", actual)); }
+                    catch {}
+            }
+        }
+    }
+    [RunInstaller(true)]
+    public class WindowsServiceInstaller : Installer
+    {
+        public WindowsServiceInstaller()
+        {
+            ServiceProcessInstaller serviceProcessInstaller = 
+                               new ServiceProcessInstaller();
+            ServiceInstaller serviceInstaller = new ServiceInstaller();
+            serviceProcessInstaller.Account = ServiceAccount.LocalSystem;
+            serviceProcessInstaller.Username = null;
+            serviceProcessInstaller.Password = null;
+            serviceInstaller.DisplayName = "Set Timer Resolution Service";
+            serviceInstaller.StartType = ServiceStartMode.Automatic;
+            serviceInstaller.ServiceName = "STR";
+            this.Installers.Add(serviceProcessInstaller);
+            this.Installers.Add(serviceInstaller);
+        }
+    }
+}
+"@
+Set-Content -Path "$env:SystemDrive\Windows\SetTimerResolutionService.cs" -Value $MultilineComment -Force
+# Compile/create service
+Start-Process -Wait "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe" -ArgumentList "-out:C:\Windows\SetTimerResolutionService.exe C:\Windows\SetTimerResolutionService.cs" -WindowStyle Hidden
+Remove-Item "$env:SystemDrive\Windows\SetTimerResolutionService.cs" -ErrorAction SilentlyContinue | Out-Null
+# Install and start service
+New-Service -Name "Set Timer Resolution Service" -BinaryPathName "$env:SystemDrive\Windows\SetTimerResolutionService.exe" -ErrorAction SilentlyContinue | Out-Null
+Set-Service -Name "Set Timer Resolution Service" -StartupType Auto -ErrorAction SilentlyContinue | Out-Null
+Set-Service -Name "Set Timer Resolution Service" -Status Running -ErrorAction SilentlyContinue | Out-Null
+# start taskmanager
+Start-Process taskmgr.exe
+exit
+
+      }
+    2 {
+
+Clear-Host
+# Stop disable delete service
+Set-Service -Name "Set Timer Resolution Service" -StartupType Disabled -ErrorAction SilentlyContinue | Out-Null
+Set-Service -Name "Set Timer Resolution Service" -Status Stopped -ErrorAction SilentlyContinue | Out-Null
+sc.exe delete "Set Timer Resolution Service" | Out-Null
+Remove-Item "$env:SystemDrive\Windows\SetTimerResolutionService.exe" -Force -ErrorAction SilentlyContinue | Out-Null
+Start-Process taskmgr.exe
+exit
+
+      }
+    } } else { Write-Host "Invalid input. Please select a valid option (1-2)." } }
+  
+}
+
+
 function Remove-AI {
   Clear-Host
   Write-Host "Removing AI/Copilot/Recall..."
@@ -5853,9 +6091,10 @@ function Start-Menu {
         Write-Host "19. Hardening - Virtualisation Security Features" -ForegroundColor Yellow
         Write-Host "20. Hardening - Defender configuration" -ForegroundColor Yellow
         Write-Host "21. Enable WSL" -ForegroundColor Yellow
-        Write-Host "22  Disable Defender/Security" -ForegroundColor Yellow
-        Write-Host "23. Remove Edge" -ForegroundColor Yellow
-        Write-Host "24. Disk cleanup" -ForegroundColor Yellow
+        Write-Host "22. Install Timer Resolution" -ForegroundColor Yellow
+        Write-Host "23. Disable Defender/Security" -ForegroundColor Yellow
+        Write-Host "24. Remove Edge" -ForegroundColor Yellow
+        Write-Host "25. Disk cleanup" -ForegroundColor Yellow
         Write-Host ""
 
         Write-Host "0.  Exit" -ForegroundColor Red
@@ -5885,9 +6124,10 @@ function Start-Menu {
             '19' { Enable-VirtualizationSecurityFeatures }
             '20' { Set-DefenderConfig }
             '21' { Enable-WSL }
-            '22' { Remove-Defender }
-            '23' { Remove-Edge }            
-            '24' { Initialize-DiskCleanup }
+            '22' { Install-TimerResolution }
+            '23' { Remove-Defender }
+            '24' { Remove-Edge }            
+            '25' { Initialize-DiskCleanup }
             '0'  { exit }
             default {
                 Write-Host "`nInvalid selection. Press Enter to try again..." -ForegroundColor Red
